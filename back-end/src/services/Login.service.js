998329fa543f994dc6e70c73bac857903b57403e
wendel <wendeldaniel263@gmail.com>
2023-05-03 18:05:31 -0300

    fix: eslint

--- back-end/src/services/Login.service.js
+++ back-end/src/services/Login.service.js
@@ -1,12 +1,12 @@
 const { users } = require('../database/models');
-const {createToken} = require('../utils/JWT.token')
+const { createToken } = require('../utils/JWT.token');
 
     const loginService = async (email, password) => {
         const data = await users.findOne({ where: { email, password } });
         if (!data) {
             return { message: 'invalid login' };
         }
-        const token = await createToken({data})
+        const token = await createToken({ data });
         return token;
     };
 
