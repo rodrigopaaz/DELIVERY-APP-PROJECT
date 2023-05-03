1ded93cc2ebd6f50f3c337e28310fe203baee358
wendel <wendeldaniel263@gmail.com>
2023-05-03 16:22:56 -0300

    feat: back-end da rota /login ok

--- back-end/src/services/Login.service.js
+++ back-end/src/services/Login.service.js
@@ -1,11 +1,13 @@
 const { users } = require('../database/models');
+const {createToken} = require('../utils/JWT.token')
 
     const loginService = async (email, password) => {
         const data = await users.findOne({ where: { email, password } });
         if (!data) {
             return { message: 'invalid login' };
         }
-        return data;
+        const token = await createToken({data})
+        return token;
     };
 
 module.exports = {
--- back-end/src/utils/JWT.token.js
+++ back-end/src/utils/JWT.token.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET || 'secret_key';
 
 const JWT_CONFIG = {
   algorithm: 'HS256',
