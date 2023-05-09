470a966551ba8e76b02bb2dc6511c22b2d451379
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-05-08 21:10:16 -0300

    fix: checkout localstorage

--- front-end/src/pages/Checkout.js
+++ front-end/src/pages/Checkout.js
@@ -16,28 +16,19 @@ export default function Checkout() {
 
   const removeItem = (id) => {
     const item = JSON.parse(localStorage.getItem('cart'));
-    const product = item.find((e) => e.id === id);
-    const deleteProduct = item.filter((e) => e.id !== id);
-    if (product.quantity > 1) {
-      const deleteItem = [...deleteProduct, { id,
-        name: product.name,
-        price: product.price,
-        quantity: product.quantity -= 1 }].sort((a, b) => a.id - b.id);
-      setCart(deleteItem);
-      localStorage.setItem('cart', JSON.stringify(deleteItem));
+    const index = item.findIndex((e) => e.id === id);
+    item[index].quantity -= 1;
+
+    if (item[index].quantity >= 1) {
+      setCart(item);
+      localStorage.setItem('cart', JSON.stringify(item));
     } else {
+      const deleteProduct = item.filter((e) => e.id !== id);
       setCart(deleteProduct);
       localStorage.setItem('cart', JSON.stringify(deleteProduct));
     }
   };
 
-  /*   const cartTest = [
-    { id: 1, name: 'teste 1', price: 2.99, quantity: 2 },
-    { id: 2, name: 'teste 2', price: 1.99, quantity: 1 },
-    { id: 3, name: 'teste 3', price: 4.99, quantity: 5 },
-    { id: 4, name: 'teste 4', price: 6.99, quantity: 8 },
-  ];
- */
   useEffect(() => {
     /*  localStorage.setItem('cart', JSON.stringify(cartTest)); */
     getSellers();
@@ -63,7 +54,7 @@ export default function Checkout() {
         </tr>
         {cart && cart.map((item, i) => (
           <tr key={ i + item.name }>
-            <td data-testid={ `element-order-table-item-number-${i}` }>{item.id}</td>
+            <td data-testid={ `element-order-table-item-number-${i}` }>{i + 1}</td>
             <td data-testid={ `element-order-table-name-${i}` }>{item.name}</td>
             <td data-testid={ `element-order-table-unit-price-${i}` }>{item.price}</td>
             <td data-testid={ `element-order-table-quantity-${i}` }>{item.quantity}</td>
